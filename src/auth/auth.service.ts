9fc290660f2c70c839ab637b50f6b14d2189cb34
Erick Vera <erickverac90@gmail.com>
2025-07-12 21:38:53 -0600

    Added logic for register and login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  UnauthorizedException,
+  ConflictException,
+} from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { LoginDto } from './dto/login.dto';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async register(createUserDto: CreateUserDto) {
+    const userExists = await this.usersService.findByEmail(createUserDto.email);
+    if (userExists) {
+      throw new ConflictException('Email already registered');
+    }
+
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+    const user = await this.usersService.create({
+      email: createUserDto.email,
+      password: hashedPassword,
+    });
+
+    return {
+      message: 'User created successfully',
+      user: { email: user.email },
+    };
+  }
+
+  async login(loginDto: LoginDto) {
+    const user = await this.usersService.findByEmail(loginDto.email);
+    if (!user || !(await bcrypt.compare(loginDto.password, user.password))) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const payload = { sub: user._id, email: user.email };
+    const token = await this.jwtService.signAsync(payload);
+
+    return { access_token: token };
+  }
+}
