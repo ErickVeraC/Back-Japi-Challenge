46d28197a8086df97c4f414d9764cb157d278010
Erick Vera <erickverac90@gmail.com>
2025-07-13 00:10:46 -0600

    Added Event schema

---
+++ src/events/schemas/event.schema.ts
@@ -0,0 +1,27 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/moongose';
+import { Document } from 'mongoose';
+
+export type EventDocument = Event & Document;
+
+@Schema({ timestamps: true })
+export class Event {
+  @Prop({ required: true })
+  title: string;
+
+  @Prop({ required: true })
+  description: string;
+
+  @Prop({ required: true })
+  date: Date;
+
+  @Prop({ required: true, min: 1 })
+  capacity: number;
+
+  @Prop({ required: true })
+  availableTickets: number;
+
+  @Prop({ required: true })
+  organizer: string;
+}
+
+export const EventSchema = SchemaFactory.createForClass(Event);
