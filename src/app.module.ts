5dfaa83a243e201ae87a446b4883814727438f0f
Erick Vera <erickverac90@gmail.com>
2025-07-12 21:01:10 -0600

    Added the User and Auth architecture to the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URI)],
+  imports: [MongooseModule.forRoot(process.env.MONGO_URI), AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
