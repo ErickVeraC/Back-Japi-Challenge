c21c955af2546c33c35d0426258cd135e03357c8
Erick Vera <erickverac90@gmail.com>
2025-07-15 21:59:30 -0600

    Added cors to vercel and .env.example

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      'https://front-japi-challenge.vercel.app',
+    ],
     credentials: true,
   });
   app.useGlobalPipes(
